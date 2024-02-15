54dd0038fcf9923fc406953264e78453e7c62495
codri <codri@VIRTUAL>
2024-02-15 22:49:26 +0200

    Add project files.

---
+++ Lab1Ex11WON7/ProgramHelpers.cs
@@ -0,0 +1,34 @@
+﻿partial class Program
+{
+    static int ReadNumber()
+    {
+        Console.WriteLine("Introduceti un numar pentru a fi clasificat");
+        var numarRaw=Console.ReadLine();
+        if(numarRaw != null && int.TryParse(numarRaw,out int numar))
+            return numar;
+        Console.WriteLine("Nu ati introdus datele in format valid, mai incercati");
+        return ReadNumber();
+    }
+    static string ClassifyNumber(int numar)
+    {
+        switch (numar)
+        {
+            case 1:
+                return "unu";
+            case 2:
+                return "doi";
+            case 3:
+                return "trei";
+            case 5:
+                return "cinci";
+            case 8:
+                return "opt";
+            default:
+                return "neidentificat";
+        }
+    }
+    static void ShowClassification(string clasificare)
+    {
+        Console.WriteLine(clasificare);
+    }
+}
